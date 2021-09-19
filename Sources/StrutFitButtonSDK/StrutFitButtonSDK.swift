d4cc0fc16263ee6369d3f67e67218cd67b6f8712
Nishij Nimmagadda <nishij61@hotmail.co.nz>
2021-09-19 17:08:58 +1200

    added function

--- Sources/StrutFitButtonSDK/StrutFitButtonSDK.swift
+++ Sources/StrutFitButtonSDK/StrutFitButtonSDK.swift
@@ -1,3 +1,7 @@
-struct StrutFitButtonSDK {
+public struct StrutFitButtonSDK {
     var text = "Hello, World!"
+    
+    public func HelloToYou () -> String {
+        return "Hello to you"
+    }
 }
