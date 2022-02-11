d3aaf0aa4dc04c42e38d0640b31584be82d4e34b
Strutfit <dev@strut.fit>
2022-02-11 17:44:21 +1300

    add pixel data

---
+++ Sources/StrutFitButtonSDK/Models/pixelData.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//
+//  Created by StrutFit Admin on 11/02/22.
+//
+
+import Foundation
+
+public struct PixelData {
+    
+    var organizationId: Int
+
+    var sfEnabled: Bool;
+
+    var orderRef: String ;
+
+    var orderValue: Float ;
+
+    var mCode: String ;
+
+    var items: String ;
+
+    var currencyCode: String ;
+
+    var domain: String ;
+    
+}
